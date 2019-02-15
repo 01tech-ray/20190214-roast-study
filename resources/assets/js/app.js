24379d1c01be17c8120e117186655a0bd32ff65d
RayZhao <fhqdnnlmnm@163.com>
2019-02-15 12:43:58 +0800

    Courese4
    配置 JavaScript 和 SASS

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,9 +5,34 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+// require('./bootstrap');
+window._ = require('lodash');
+try {
+    window.$ = window.jQuery = require('jquery');
+    require('foundation-sites');
+} catch (e) {}
+// window.Popper = require('popper.js').default;
 
-window.Vue = require('vue');
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Next we will register the CSRF Token as a common header with Axios so that
+ * all outgoing HTTP requests automatically have it attached. This is just
+ * a simple convenience so we don't have to attach every token manually.
+ */
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
+
+// window.Vue = require('vue');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,7 +40,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
     el: '#app'
